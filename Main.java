b5ea1b805531ed3fe7285a893aa54b0e3ad79657
Morgan jully <jully.morgan@orange.fr>
2017-11-07 13:57:11 +0100

    test

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class Main {
+	public static void main(String[] args) {
+		Menu menu = new Menu();
+	}
+}
